1ca682fbc5b76275d419682fa5d859be135e7716
wdgan <18149542718@163>
2017-08-30 14:14:49 +0800

    MVp
    mvp up

---
+++ Chat/app/src/main/java/mvp/contract/MainContract.java
@@ -0,0 +1,16 @@
+package mvp.contract;
+
+/**
+ * Created by ${wdgan} on 2017/8/30 0030.
+ * 邮箱18149542718@163
+ */
+public interface MainContract {
+    interface Model {
+    }
+
+    interface View {
+    }
+
+    interface Presenter {
+    }
+}
---
+++ Chat/app/src/main/java/mvp/presenter/MainPresenter.java
@@ -0,0 +1,10 @@
+package mvp.presenter;
+
+import mvp.contract.MainContract;
+
+/**
+ * Created by ${wdgan} on 2017/8/30 0030.
+ * 邮箱18149542718@163
+ */
+public class MainPresenter implements MainContract.Presenter {
+}
